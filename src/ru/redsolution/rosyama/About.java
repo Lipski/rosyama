ff3a3aa313332c7879a0f5e157c27d97630b88a2
Alexander Ivanov <alexander.vl.ivanov@gmail.com>
2011-09-06 11:56:21 +0600

    Make link to the source code clickable.

--- src/ru/redsolution/rosyama/About.java
+++ src/ru/redsolution/rosyama/About.java
@@ -1,14 +1,19 @@
 package ru.redsolution.rosyama;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
 
-public class About extends Activity {
+public class About extends Activity implements OnClickListener {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.about);
+		findViewById(R.id.about_source).setOnClickListener(this);
 	}
 
 	@Override
@@ -20,4 +25,13 @@ public class About extends Activity {
 		InterfaceUtilities.setTiledBackground(this, R.id.dot2, R.drawable.dot);
 	}
 
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.about_source:
+			startActivity(new Intent(Intent.ACTION_VIEW,
+					Uri.parse(getString(R.string.about_source_address))));
+			break;
+		}
+	}
 }
